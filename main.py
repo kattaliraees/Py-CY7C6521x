37d54517c52e9ce236db7b6543ad84cbc1daeb0d
Raees Kattali <kattaliraees@gmail.com>
2021-08-16 13:55:17 +0300

    test

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+#GPIO Test
+
 from device import CyUSBSerial, CyGPIO
+
 def main():
+    print("Starting...")
+
+    #below line will throw error if driver is not installed
     lib = CyUSBSerial(lib="cyusbserial")
+
+    #Getting the connected Cy7c65215 device
     dev = lib.find().next()
-    print("Hello World!")
-    print(dev)
+
+    #GPIO Test
     gpio = CyGPIO(dev)
-    print(gpio)
+    gpio.set(5, 1)
+    gpio.get(5)
 
 if __name__ == "__main__":
     main()
